d0e7cf2f7aa1741cafaa52d96ff3fae66b4ff6a9
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-04 13:58:00 +0200

    day 02 ex00

---
+++ day02/ex00/another_world.php
@@ -0,0 +1,15 @@
+<?php
+	if ($argc > 1)
+	{
+		$trim = trim($argv[1], " ");
+		$str = explode(" ", $trim);
+
+		foreach ($str as $word)
+		{
+			if (end($str) == $word)
+				echo "$word\n";
+			elseif ($word)
+				echo "$word ";
+		}
+	}
+?>
