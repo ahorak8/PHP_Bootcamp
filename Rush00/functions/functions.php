83a9a0f302cdada8a7d393aab4842a84c9d89638
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-07 00:48:23 +0200

    get products info for home page function

--- Rush00/functions/functions.php
+++ Rush00/functions/functions.php
@@ -19,4 +19,30 @@ function getCats() {
 	}
 }
 
+function getPro () {
+	
+	global $con;
+	$get_pro = "select * from products order by RAND() LIMIT 0,6";
+	$run_pro = mysqli_query($con, $get_pro);
+
+	while ($row_pro =mysqli_fetch_array($run_pro)) {
+		
+		$pro_id = $row_pro['product_id'];
+		$pro_cat = $row_pro['product_cat'];
+		$pro_title = $row_pro['product_title'];
+		$pro_price = $row_pro['product_price'];
+		$pro_image = $row_pro['product_image'];
+
+		echo "
+			<div id='single_product'>
+				<h3>$pro_title</h3>
+				<img src='admin_area/product_images/$pro_image' width='180' height='180' />
+				<p><b> R $pro_price </b></p>
+				<a href='details.php?pro_id=$pro_id' style='float:left';>Details</a>
+				<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</buton></a>
+			</div>
+		";
+	}
+}
+
 ?>
