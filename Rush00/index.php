5ed8d3c3a7abbe6385506a78af2233cfebc787aa
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-06 16:09:24 +0200

    Rush00 basis

---
+++ Rush00/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title>RushedCosplay</title>
+
+	<link rel="stylesheet" href="styles/style.css" media="all" />
+</head>
+<body>
+	<!--Main Container starts here -->
+	<div class="main_wrapper">
+		<!--Header starts here -->
+		<div class="header_wrapper">
+			<img id="logo" src="images/logo.png"/>
+			<img id="banner" src="images/banner.jpg"/>
+		</div>
+		<!--Header ends here -->
+
+		<!--Menu bar starts here -->
+				<div class="menubar">
+					<ul id="menu">
+						<li><a href="#">Home</a></li>
+						<li><a href="#">All Products</a></li>
+						<li><a href="#">My Account</a></li>
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Shopping Cart</a></li>
+					</ul>
+				</div>
+		<!--Menu bar ends here -->
+
+		<div class="content_wrapper"> 
+			<div id="sidebar">Sidebar</div>
+			<div id="content_area">Content area</div>
+		</div>
+		<div id="footer">Footer</div>
+	</div>
+	<!--Main Container ends here -->
+</body>
+</html>
