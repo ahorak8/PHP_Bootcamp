6370dd585c53557a2cf87aaae767adb84114a274
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-06 22:43:14 +0200

    Successfully able to insert product to database using insert_product.php

--- Rush00/admin_area/insert_product.php
+++ Rush00/admin_area/insert_product.php
@@ -69,7 +69,7 @@ include("includes/db.php");
 </html>
 
 <?php
-	if (isset($_POST['insert_post'])) {
+	if(isset($_POST['insert_product'])) {
 
 		//getting the text data from the form fields
 		$product_title = $_POST['product_title'];
@@ -84,11 +84,11 @@ include("includes/db.php");
 
 		move_uploaded_file($product_image_tmp, "product_images/$product_image");
 
-		$insert_product = "insert into products (product_cat, product_title, product_price, product_desc, product_image, product_keywords) values ('$product_id', '$product_cat', '$product_title', '$product_price', '$product_desc', '$product_image', '$product_keywords')";
+		$insert_product = "insert into products (product_cat, product_title, product_price, product_desc, product_image, product_keywords) values ('$product_cat', '$product_title', '$product_price', '$product_desc', '$product_image', '$product_keywords')";
 
 		$insert_pro = mysqli_query($con, $insert_product);
 
-		if ($insert_pro) {
+		if($insert_pro) {
 			echo "<script>alert('Product has been inserted!')</script>";
 			echo "<script>window.open('insert_product.php', '_self')</script>";
 		}
