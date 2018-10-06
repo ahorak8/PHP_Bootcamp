c89027d75c9139add613a1c346d5f38fa6636b4a
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-06 16:32:09 +0200

    Search bar

--- Rush00/index.php
+++ Rush00/index.php
@@ -15,7 +15,7 @@
 		</div>
 		<!--Header ends here -->
 
-		<!--Menu bar starts here -->
+				<!--Menu bar starts here -->
 				<div class="menubar">
 					<ul id="menu">
 						<li><a href="#">Home</a></li>
@@ -25,11 +25,21 @@
 						<li><a href="#">Contact Us</a></li>
 						<li><a href="#">Shopping Cart</a></li>
 					</ul>
+					<!-- Search bar starts here -->
+					<div id="form">
+						<form method="get" action="results.php" enctype="multipart/form-data">
+						<input type="text" name="user_query" placeholder="Search a Product"/>
+						<input type="submit" name="search" value="Search" />
+						</form>
+					</div>
+					<!-- Search bar ends here -->
 				</div>
-		<!--Menu bar ends here -->
+				<!--Menu bar ends here -->
 
 		<div class="content_wrapper"> 
-			<div id="sidebar">Sidebar</div>
+			<div id="sidebar">
+				<div id="sidebar_title">Categories</div>
+			</div>
 			<div id="content_area">Content area</div>
 		</div>
 		<div id="footer">Footer</div>
