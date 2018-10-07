64b504c49ad0becd4c8b6e707fdf0ad99e109db2
aleksiel HORAK <ahorak@c2r4s1.wethinkcode.co.za>
2018-10-07 09:57:00 +0200

    cart functions

--- Rush00/functions/functions.php
+++ Rush00/functions/functions.php
@@ -155,4 +155,84 @@ function getCat2Pro () {
 	}
 }
 
+//Get Ip Address
+function getIp() {
+    $ip = $_SERVER['REMOTE_ADDR'];
+ 
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+ 
+    return $ip;
+}
+
+//create cart
+function cart() {
+	if (isset($_GET['add_cart'])) {
+		global $con;
+		$ip = getip();
+		$pro_id = $_GET['add_car'];
+		
+		$check_pro = "select * from cart where ip_add='$ip' AND p_id='$pro_id'";
+		$run_check = mysqli_query($con, $check_pro);
+		if(mysqli_num_rows($run_check)>0){
+		
+			echo "";
+		}
+		else {
+		
+			$insert_pro = "insert into cart (p_id, ip_add) values ('$p_id','$ip')";
+		
+			$run_pro = mysqli_query($con, $insert_pro);
+			echo "<script>window.open('index.php','_self')</script>";
+		}
+	}
+}
+
+//Total items in cart
+function total_items(){
+    
+    if(iset($_GET['add_cart'])){
+        global $con;
+        $ip = getIp();
+        $get_items ="select * from cart where ip_add='$ip'";
+        $run_items = mysqli_query($con, $get_items);
+        $count_items = mysqli_num_rows($run_items);
+        
+        }
+        
+        else {
+        
+        $ip = getIp();
+        $get_items ="select * from cart where ip_add='$ip'";
+        $run_items = mysqli_query($con, $get_items);
+        $count_items = mysqli_num_rows($run_items);
+        }
+    echo $count_items;
+}
+
+//Get price total 
+function total_price(){
+    $total = 0;
+    global $con;
+    $ip = getIp();
+    $sel_price = "select * from cart where ip_add='$ip'";
+    $run_price = mysqli_query($con, $sel_price);
+    while($p_price=mysqli_fetch_array($run_price)){
+        $pro_id = $p_price['p_id'];
+        $pro_price = "select * from products where product_id='$pro_id'";
+        $run_pro_price = mysqli_query($con, $pro_price);
+        while ($pp_price = msqli_fetch_array($run_pro_price)){
+        
+        $product_price = array($pp_price['product_price']);
+        $values = array_sum($product_price);
+        $total += $values;
+        
+        }
+    }
+    echo "R" .$total;
+}
+
 ?>
